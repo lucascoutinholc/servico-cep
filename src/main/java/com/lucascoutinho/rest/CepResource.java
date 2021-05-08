8e5bc1781ff6c137183e4afd0101f18afd7494a2
Lucas Coutinho <lukascoutinhoalmeida@gmail.com>
2021-05-08 15:55:56 -0300

    Changing classes and removing tests packages

--- src/main/java/com/lucascoutinho/rest/CepResource.java
+++ src/main/java/com/lucascoutinho/rest/CepResource.java
@@ -1,12 +1,12 @@
-package com.lucascoutinho;
+package com.lucascoutinho.rest;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("/hello-resteasy")
-public class GreetingResource {
+@Path("/cep")
+public class CepResource {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
